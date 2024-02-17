3c229f06ed72a66c2d56183a0d07dcbee9e70770
Vítor Santos <jvitor.ti03@gmail.com>
2024-02-17 16:08:10 -0300

    CAREGANDO OS MODELS

--- assets/node-str/src/app.js
+++ assets/node-str/src/app.js
@@ -12,6 +12,9 @@ const router = express.Router();
 // CONEXÃO COM O BANCO DE DADOS - HrrKcAYMk6ZRhv7p
 mongoose.connect('mongodb+srv://jvitorti03:HrrKcAYMk6ZRhv7p@cluster0.7ehwal0.mongodb.net/?retryWrites=true&w=majority');
 
+// CARREGA OS MODELS
+const Product = require('.models/product');
+
 // CARREGA AS ROTAS
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
