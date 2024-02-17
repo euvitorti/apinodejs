68f4972a751cab4f296ddabe644a5dbecf13685f
Vítor Santos <jvitor.ti03@gmail.com>
2024-02-17 15:53:13 -0300

    Schema

---
+++ assets/node-str/src/models/product.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        //TRIM - REMOVE OS ESPAÇOS ANTES E DEPOIS
+        trim: true
+    },
+    slug: { // CADEIRA GAMER = cadeira-gamer (URL, NÃO PODE SER REPETIDO)
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags: [{
+        type: String,
+        required: true
+    }]
+});
+
+module.exports = mongoose.model('Product', schema);
