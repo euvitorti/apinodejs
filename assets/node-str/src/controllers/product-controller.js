9b53241b4d4a2470c823fba380822d971d141ee8
Vítor Santos <jvitor.ti03@gmail.com>
2024-02-24 19:23:34 -0300

    ENDENTAÇÃO

--- assets/node-str/src/controllers/product-controller.js
+++ assets/node-str/src/controllers/product-controller.js
@@ -22,7 +22,9 @@ exports.post = (req, res, next) => {
     product
         .save()
         .then(x => {
-            res.status(201).send({ message: 'Produto cadastrado com sucesso!' });
+            res.status(201).send({
+                message: 'Produto cadastrado com sucesso!'
+            });
         }).catch(e => {
             res.status(400).send({
                 message: 'Falha ao cadastrar o produto!',
