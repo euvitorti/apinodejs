63e46fe9c6851d01354f68b44a9025063f7e32d6
Vítor Santos <jvitor.ti03@gmail.com>
2024-03-02 22:29:49 -0300

    CONSULTAR POR TAG, ID E DAR O UPDATE

--- assets/node-str/src/controllers/product-controller.js
+++ assets/node-str/src/controllers/product-controller.js
@@ -17,6 +17,45 @@ exports.get = (req, res, next) => {
         });
 };
 
+// LISTANDO PELO SLUG
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+
+// LISTANDO PELO ID
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+
+// LISTANDO PELA TAG
+exports.getByTag = (req, res, next) => {
+    Product
+        .find({
+            tags: req.params.tag,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
     product
@@ -34,11 +73,24 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price,
+                slug: req.body.slug
+            }
+        }).then(x => {
+            res.status(201).send({
+                message: "PRODUTO ATUALIZADO COM SUCESSO!"
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: "FALHA AO ATUALIZAR O PRODUTO",
+                data: e
+            });
+        });
 };
 
 exports.delet = (req, res, next) => {
