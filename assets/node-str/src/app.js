21edba82052aa6b0c50ec5841de340c9149866ba
Vítor Santos <jvitor.ti03@gmail.com>
2024-03-10 18:54:27 -0300

    SEU BANCO DE DADOS

--- assets/node-str/src/app.js
+++ assets/node-str/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 const router = express.Router();
 
 // CONECTA AO BANCO - SUA URL PARA O MONGODB
-mongoose.connect('mongodb+srv://jvitorti03:4YYNgMMjhhy7FbXj@cluster0.av48chp.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect();
 
 // CARREGA OS MODELS
 const Product = require('./models/product');
