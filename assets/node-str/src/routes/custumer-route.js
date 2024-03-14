d239519a7fbc332285c9a9b26229c7b121886fb6
Vítor Santos <jvitor.ti03@gmail.com>
2024-03-14 15:44:12 -0300

    NOME DO ARQUIVO INCORRETO

--- assets/node-str/src/routes/custumer-route.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/custumer-controller');
-
-router.post('/', controller.post);
-
-module.exports = router;
