5b8eab93310f4d303ca52bc816319d94b73530ef
Vítor Santos <jvitor.ti03@gmail.com>
2024-03-18 14:48:23 -0300

    Grid 2.0.0

--- assets/node-str/src/config.js
+++ assets/node-str/src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = '';
+global.SALT_KEY = 'sue chave do servidor';
 global.EMAIL_TMPL = '<strong>{0}</strong>';
 
 module.exports = {
-    connectionString: '',
-    sendgridkey: 'Site sendGrid - Crie uma chave api, copie e cole aqui',
+    connectionString: 'url do banco de dados',
+    sendgridkey: 'site sendgrid - cria uma cahve api, cola aqui',
     containerConnectionString: 'TBD'
 };
