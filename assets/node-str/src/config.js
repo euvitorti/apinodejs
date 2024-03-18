7f6e0f2fad91eeefbf0eb793b5e5418504e86c79
Vítor Santos <jvitor.ti03@gmail.com>
2024-03-18 14:21:56 -0300

    biblioteca md5 e segurança das senhas

--- assets/node-str/src/config.js
+++ assets/node-str/src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'chave do servidor';
+global.SALT_KEY = 'sua chave do servidor';
 global.EMAIL_TMPL = '<strong>{0}</strong>';
 
 module.exports = {
-    connectionString: 'sua string de conexão do banco de dados',
+    connectionString: 'usa url do banco de dados',
     sendgridkey: 'TBD',
     containerConnectionString: 'TBD'
 };
