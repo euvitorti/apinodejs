c1b18ada2d5be29902796afd30df2718624ebcd7
Vítor Santos <jvitor.ti03@gmail.com>
2024-03-19 16:09:09 -0300

    Mensagem de boas vindas, Métodos get e delete do customer

--- assets/node-str/src/controllers/customer-controller.js
+++ assets/node-str/src/controllers/customer-controller.js
@@ -4,6 +4,8 @@ const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 const md5 = require('md5');
 
+const emailService = require('../service/email-service');
+
 exports.post = async (req, res, next) => {
     let contract = new ValidationContract();
     contract.hasMinLen(req.body.name, 3, 'O NOME DEVE CONTER PELO MENOS 3 CARACTERES');
@@ -22,12 +24,43 @@ exports.post = async (req, res, next) => {
             email: req.body.email,
             password: md5(req.body.password + global.SALT_KEY)
         });
+
+        emailService.send(
+            req.body.email,
+            'Welcome! It`s a good choice!',
+            global.EMAIL_TMPL.replace('{0}', req.body.name));
+
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso!'
         });
     } catch (e) {
         res.status(500).send({
             message: 'Falha ao cadastrar o cliente.'
+        });
+    };
+};
+
+exports.delete = async (req, res, next) => {
+    try {
+        await repository.delete(req.body.id)
+        res.status(200).send({
+            message: "CLIENTE DELETADO COM SUCESSO!"
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'FALHA AO DELETAR O CLIENTE.'
+        });
+    };
+};
+
+exports.get = async (req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+         
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição.'
         });
     };
 };
