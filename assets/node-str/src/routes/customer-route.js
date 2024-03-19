eb0ebd2eff8d39cfe58598ffec5a3aafbd44b5bb
Vítor Santos <jvitor.ti03@gmail.com>
2024-03-19 16:09:42 -0300

    Rota delete e get do customer

--- assets/node-str/src/routes/customer-route.js
+++ assets/node-str/src/routes/customer-route.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/customer-controller');
 
 router.post('/', controller.post);
+router.delete('/', controller.delete);
+router.get('/', controller.get);
 
 module.exports = router;
