cf1e1a0a8aa63b01ce7e63998b0e9d4906c83f81
Vítor Santos <jvitor.ti03@gmail.com>
2024-03-20 15:03:03 -0300

    email

--- assets/node-str/src/service/email-service.js
+++ assets/node-str/src/service/email-service.js
@@ -1,11 +1,11 @@
 'use strict';
 var config = require('../config');
-var sendGrid = require('sendGrid')(config.sendgridkey);
+var sendGrid = require('sendgrid')(config.sendgridkey);
 
 exports.send = async (to, subject, body) => {
     sendGrid.send({
         to: to,
-        from: 'email de origem',
+        from: 'email',
         subject: subject,
         html: body
     });
